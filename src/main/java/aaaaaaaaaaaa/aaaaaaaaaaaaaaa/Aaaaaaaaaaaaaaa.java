94e2db99b54d7f0606e18b40fd687a2b9c1bfa64
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-10-31 09:21:56 +0900

    更新

--- src/main/java/aaaaaaaaaaaa/aaaaaaaaaaaaaaa/Aaaaaaaaaaaaaaa.java
+++ src/main/java/aaaaaaaaaaaa/aaaaaaaaaaaaaaa/Aaaaaaaaaaaaaaa.java
@@ -1,36 +1,37 @@
 package aaaaaaaaaaaa.aaaaaaaaaaaaaaa;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 public final class Aaaaaaaaaaaaaaa extends JavaPlugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
-        getLogger().info("Hello わーーーーーーーーーーーーーーーーーーーーーーーーーるど");
+        getLogger().info("start plugin");
+
     }
+
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        getLogger().info("test disable");
+
+    }
+    /**
+     * コマンド実行処理
+     */
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args){
+        // test コマンドの処理
+        if(cmd.getName().equalsIgnoreCase("test")){
+            // 取りあえずログ出力して終わる
+            getLogger().info("テストコマンドを実行しました");
+        }
+        // 該当するコマンド無し
+        return false;
     }
 }
