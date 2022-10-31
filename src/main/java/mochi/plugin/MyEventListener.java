26e41abc490a37bdcfcfe320c6d81966209df136
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-10-31 20:34:51 +0900

    スニークしたら花火が上がるようにした

--- src/main/java/mochi/plugin/MyEventListener.java
+++ src/main/java/mochi/plugin/MyEventListener.java
@@ -1,8 +1,40 @@
 package mochi.plugin;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 
-public class MyEventListener  implements Listener {
+import java.util.Objects;
+
+public class MyEventListener implements Listener {
+    /**
+     * プレイヤーが入ったら動く
+     * @param event イベント
+     */
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Bukkit.broadcastMessage("welcome " + event.getPlayer().getName() + " to my server");
+
+    }
+
+    @EventHandler
+    public void onPlayerneak(PlayerToggleSneakEvent event ) {
+
+        if (!event.getPlayer().isSneaking()) {
+
+//            Bukkit.broadcastMessage("スニークしたぞ スニークの状態" + event.getPlayer().isSneaking());
+            World world = Bukkit.getWorld(event.getPlayer().getWorld().getUID())
+                    ;
+            Location PlyerLocation = event.getPlayer().getLocation();
+            Bukkit.broadcastMessage(String.valueOf(PlyerLocation.getX()));
+            Objects.requireNonNull(world).spawnEntity(PlyerLocation, EntityType.FIREWORK);
+        }
+    }
 
 }
 
