a1199841dae31c99e70ab4869b797ac224ff0097
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2022-11-02 13:53:50 +0900

    スニークしたら花火が上がるようにした

--- src/main/java/mochi/plugin/MyEventListener.java
+++ src/main/java/mochi/plugin/MyEventListener.java
@@ -2,14 +2,16 @@ package mochi.plugin;
 
 import java.util.Objects;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
+import org.bukkit.*;
+import org.bukkit.block.data.type.Fire;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Firework;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
+import org.bukkit.inventory.meta.FireworkMeta;
 
 public class MyEventListener implements Listener {
     /**
@@ -27,14 +29,20 @@ public class MyEventListener implements Listener {
     public void onPlayerSneak(PlayerToggleSneakEvent event) {
 
         if (!event.getPlayer().isSneaking()) {
-
-            Bukkit.broadcastMessage("スニークしたぞ スニークの状態" +
-                    event.getPlayer().isSneaking());
+//            Bukkit.broadcastMessage("スニークしたぞ スニークの状態" +
+//                    event.getPlayer().isSneaking());
             World world = Bukkit.getWorld(event.getPlayer().getWorld().getUID());
             Location PlayerLocation = event.getPlayer().getLocation();
             Bukkit.broadcastMessage(String.valueOf(PlayerLocation.getX()));
-            EntityType FireWork = EntityType.FIREWORK;
-            Objects.requireNonNull(world).spawnEntity(PlayerLocation, FireWork);
+
+            Bukkit.broadcastMessage(String.valueOf(PlayerLocation.getY()));
+            PlayerLocation.add(3, 3, 3).setYaw(0);
+            PlayerLocation.setPitch(20);
+            Firework FireWokRoket = (Firework) Objects.requireNonNull(world).spawnEntity(PlayerLocation, EntityType.FIREWORK);
+            FireworkMeta data = FireWokRoket.getFireworkMeta();
+            data.addEffect(FireworkEffect.builder().withColor(Color.GREEN).withFlicker().flicker(true).withColor(Color.BLACK).with(FireworkEffect.Type.BALL_LARGE).build());
+            data.setPower(2);
+            FireWokRoket.setFireworkMeta(data);
         }
     }
 }
--- startServer.sh
+++ startServer.sh
@@ -1,2 +1,2 @@
-#java -jar -Xms1024M -Xmx1024M paper-1.19.2-246.jar --nogui
+# java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -jar -Xms1024M -Xmx1024M paper-1.19.2-246.jar --nogui
 java -jar -XX:+UseG1GC -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M ./test/paper-1.19.2-246.jar --nogui
